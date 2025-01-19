5845c0b61113623f1baf9d6a12821629a595180b
Ethan Yan <eyyan@wisc.edu>
2025-01-18 21:18:37 -0500

    Initial Commit

---
+++ cs8803/cs8803App.swift
@@ -0,0 +1,17 @@
+//
+//  cs8803App.swift
+//  cs8803
+//
+//  Created by Ethan Yan on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct cs8803App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cs8803Tests/cs8803Tests.swift
@@ -0,0 +1,17 @@
+//
+//  cs8803Tests.swift
+//  cs8803Tests
+//
+//  Created by Ethan Yan on 18/1/25.
+//
+
+import Testing
+@testable import cs8803
+
+struct cs8803Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
